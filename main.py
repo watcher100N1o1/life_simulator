1cebbf15187a9f3c33ad537964fa7340cc05fb1c
flesh0of0time <nk@Neot.Mchn_spir.com>
2026-06-20 23:47:15 +0300

    food-chain-01

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
+"""**Основной файл**
+
+Рабочее тело проекта"""
+
+
+#Импортируем модули
 from ecosystem import Ecosystem
-from organism import Organism
+from organism import Plant, Predator, Herbivore, Populy
+
+
 
 def main():
-   eco = Ecosystem()
-   rabbit = Organism("Заяц", 20)
-   fox = Organism("Лиса", 30)
+    """**Основная функция**
+    
+    Рабочее тело проекта"""
 
-   eco.add_organism(rabbit)
-   eco.add_organism(fox)
 
-   eco.simulate_day()
+    eco = Ecosystem()  #Экосистема
+    plants = Populy()  #Популяция растений
+    preds = Populy()  #Популяция хищников
+    herbs = Populy()  #Популяция травоядных
+    #Заяц - травоядное
+    rabbit = Herbivore("Заяц", 20, 15)  
+    #Лиса - хищник
+    fox = Predator("Лиса", 30, 20)  
+    #Клубника - растение
+    strawberry = Plant("Клубника", 50)  
 
+    #Добавляем существ в экосистему и соответствующие популяции
+    eco.add_organism(strawberry, plants) 
+    eco.add_organism(rabbit, herbs)
+    eco.add_organism(fox, preds)
+
+    #Симулируем день
+    eco.simulate_day()
+
+    #Выводим количество существ в популяциях после дня:
+    print(f"Растений: {plants.populy}")
+    print(f"Хищников: {preds.populy}")
+    print(f"Травоядных: {herbs.populy}")
+
+#Запускаем основную функцию:
 if __name__ == "__main__":
-   main()
+    main()
